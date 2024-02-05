fa4677411a4de0f9dd7be0f8fb5b47dbf95db3c4
Juan Gómez <gomezj01@stepstone.com>
2024-02-05 11:26:22 -0600

    Uso de use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+// "use client"
+
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log('Hello world! :), main page')
+
   return (
     <main className={styles.main}>
       <h1>Hello World</h1>
