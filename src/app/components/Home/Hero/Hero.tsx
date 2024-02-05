e3ffb10aeebf3b0a215bf614d5d29669c1beca3d
Juan Gómez <gomezj01@stepstone.com>
2024-02-05 13:19:10 -0600

    CSS module

--- src/app/components/Home/Hero/Hero.tsx
+++ src/app/components/Home/Hero/Hero.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import styles from './Hero.module.css'
 
 export const Hero = () => {
   return (
-    <section>
-      <h1>Hero</h1>
+    <section className={ styles.Hero }>
+      <h1>Future World</h1>
+      <h2>Empowering your tomorrow, today!</h2>
     </section>
   )
 }
