c6b3da033105a8bbd7e5ce8b12efb3b2f9070c9d
Juan Gómez <gomezj01@stepstone.com>
2024-02-05 14:50:47 -0600

    Diseño del componente Description

--- src/components/Home/Description/Description.tsx
+++ src/components/Home/Description/Description.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
+import styles from './Description.module.sass'
 
 export const Description = () => {
   return (
-    <section>
+    <section className={ styles.Description }>
 			<Image src='/images/description.jpeg' alt='products marketplace' width={320} height={320}/>
-			<h2>Description</h2>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet perferendis soluta earum voluptate reprehenderit eius nostrum sed labore nam, vel vero quod! Sint ipsam quas deserunt voluptatibus, odit error dolor!</p>
+			<div className={ styles.Description__text }>
+				<h2>Bring the future today</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet perferendis soluta earum voluptate reprehenderit eius nostrum sed labore nam, vel vero quod! Sint ipsam quas deserunt voluptatibus, odit error dolor!</p>
+			</div>
 		</section>
   )
 }
