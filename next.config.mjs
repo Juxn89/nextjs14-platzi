265eaedd5899ae80a2a7a75f8ae391ded5709fe5
Juan Gómez <gomezj01@stepstone.com>
2024-02-05 13:52:19 -0600

    Uso de SASS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+import path from 'path';
+import { fileURLToPath } from 'url'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const nextConfig = {
+    sassOptions: {
+        includePaths: [ path.join(__dirname, 'src/sass') ],
+        prependData: `@import "main.sass"`
+    }
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { Description } from "./components/Home/Description";
-import { Hero } from "./components/Home/Hero";
-import { MainProducts } from "./components/Home/MainProducts";
+import { Description } from "@/components/Home/Description";
+import { Hero } from "@/components/Home/Hero";
+import { MainProducts } from "@/components/Home/MainProducts";
 
 export default function Home() {
   console.log('Hello world! :), main page')
