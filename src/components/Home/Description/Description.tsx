7dc0e9cb0a52ade74f066fe57731567a68121220
Juan Gómez <gomezj01@stepstone.com>
2024-02-05 14:46:29 -0600

    Archivos estáticos

--- src/components/Home/Description/Description.tsx
+++ src/components/Home/Description/Description.tsx
@@ -1,5 +1,11 @@
+import Image from 'next/image'
+
 export const Description = () => {
   return (
-    <div>Description</div>
+    <section>
+			<Image src='/images/description.jpeg' alt='products marketplace' width={320} height={320}/>
+			<h2>Description</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet perferendis soluta earum voluptate reprehenderit eius nostrum sed labore nam, vel vero quod! Sint ipsam quas deserunt voluptatibus, odit error dolor!</p>
+		</section>
   )
 }
