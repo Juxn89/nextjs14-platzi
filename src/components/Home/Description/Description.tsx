71b8279a98a27abfd06d4e5a9cabc05ef3041b33
Juan Gómez <gomezj01@stepstone.com>
2024-02-06 13:41:06 -0600

    Estilos dinámicos con ClassNames

--- src/components/Home/Description/Description.tsx
+++ src/components/Home/Description/Description.tsx
@@ -1,13 +1,28 @@
+'use client'
+
 import Image from 'next/image'
+import { useState } from 'react'
+import classNames from 'classnames/bind'
 import { PLACEHOLDER_IMAGE_BLUR } from '@/common'
 import styles from './Description.module.sass'
 
 export const Description = () => {
+	const [hasBorder, setBorder] = useState(false);
+	const handleClick = () => setBorder(!hasBorder)
+
+	const context = classNames.bind(styles)
+	const buttonStyles = context('Description__button', {
+		'Description__button--border': hasBorder
+	})
+
   return (
     <section className={ styles.Description }>
-			<div className={ styles.Description__imageContainer }>
-				<Image src='/images/description.jpeg' alt='products marketplace' fill placeholder='blur' blurDataURL={ PLACEHOLDER_IMAGE_BLUR }/>
-			</div>
+			<button onClick={ handleClick } className={ buttonStyles }>
+				<div className={ styles.Description__imageContainer }>
+					<Image src='/images/description.jpeg' alt='products marketplace' fill placeholder='blur' blurDataURL={ PLACEHOLDER_IMAGE_BLUR }/>
+				</div>
+			</button>
+
 			<div className={ styles.Description__text }>
 				<h2>Bring the future today</h2>
 				<p>
