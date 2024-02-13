d6ca531f15508907d0b8595c90a37b44fa3aab0f
Juan Gómez <gomezj01@stepstone.com>
2024-02-13 10:01:38 -0600

    Mejorando SEO de una página

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,17 @@
+import { Metadata } from "next";
 import { MainProducts } from "@/components/Home/MainProducts";
+
+export const metadata: Metadata = {
+	title: '✨ Future world',
+	description: 'Welcome to the future world, an ecommerce from other century',
+	keywords: [ 'ecommerce', 'future', 'world', 'technology' ]
+}
 
 export default function Home() {
   console.log('Hello world! :), main page (React Server Component)')
 
   return (
     <main>
-			<h1>Products</h1>
       <MainProducts />
     </main>
   );
--- src/app/products/[handle]/page.tsx
+++ src/app/products/[handle]/page.tsx
@@ -8,6 +8,21 @@ interface ProductPageProps {
 	}
 }
 
+export async function generateMetadata( { searchParams }: ProductPageProps) {
+	const { id } = searchParams
+	const products = await getProducts(id)
+	const product = products[0]
+
+	return{
+		title: product.title,
+		description: product.description,
+		keywords: product.tags,
+		openGraph: {
+			images: [ product.image ]
+		}
+	}
+}
+
 export default async function ProductPage(props: ProductPageProps) {
 	// const params = useParams()
 	// const searchParams = useSearchParams()
