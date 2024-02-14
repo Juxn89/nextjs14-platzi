5cf51d46c78ba46798f7e036c05b20e1897cee0e
Juan Gómez <gomezj01@stepstone.com>
2024-02-14 14:35:05 -0600

    Creación de User de Shopify con GraphQL

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,21 @@
-'use server'
+"use server"
+import { GraphQLClientSingleton } from "@/graphql"
+import { createUserMutation } from "@/graphql/mutations/createUserMutation"
+import { RequestDocument } from "graphql-request"
 
-export const handleCreateUser = (formData) => {
-	console.log(formData)
+export const handleCreateUser = async (formData: FormData) => {
+  const formDataObject = Object.fromEntries(formData)
+  delete formDataObject["password_confirmation"]
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+  const variables = {
+    input: {
+      ...formDataObject,
+      phone: '+505' + formDataObject.phone
+    }
+  }
+
+  const { customerCreate } = await graphqlClient.request(createUserMutation, variables)
+  const { customerUserErrors, customer } = customerCreate
+  console.log(customer)
+  console.log(customerUserErrors)
 }
