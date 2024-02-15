befdce310fef11e397a0d230bfe1988ceea1e2e9
Juan Gómez <gomezj01@stepstone.com>
2024-02-15 13:11:40 -0600

    Implementando el carrito de compras

--- src/components/Shared/Header/header.tsx
+++ src/components/Shared/Header/header.tsx
@@ -1,29 +1,30 @@
-import Link from "next/link"
-import styles from './Header.module.css'
-import { cookies } from "next/headers"
-import { validateAccessToken } from "@/utils/auth/validateAccessToken"
+import Link from 'next/link'
+import { validateAccessToken } from '@/utils/auth/validateAccessToken'
+import { ShoppingCart } from '@/components/Shared/ShoppingCart'
+import styles from './Header.module.sass'
 
 export const Header = async () => {
-	console.log('Hello from Header (Server component)')
+  const customer = await validateAccessToken()
 
-	const customer = await validateAccessToken();
-
-	return(
-		<header>
-			<nav>
-				<ul className={ styles.Header__list }>
-					<Link href="/">
-						<li>Home</li>
-					</Link>
-					<Link href="/store">
-						<li>Store</li>
-					</Link>
-					<Link href="/test">
-						<li>Test</li>
-					</Link>
-				</ul>
-				{ customer ? (<>Hello { customer.firstName }! 🙂</>) : <Link href='/login'>Login</Link> }
-			</nav>
-		</header>
-	)
+  return (
+    <header className={styles.Header}>
+      <nav>
+        <ul className={styles.Header__list}>
+          <li>
+            <Link href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/store">
+              Store
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.Header__user}>
+        {customer?.firstName ? (<p>Hola! {customer.firstName} 🙂</p>) : (<Link href="/login">Login</Link>)}
+				<ShoppingCart />
+      </div>
+    </header>)
 }
