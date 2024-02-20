d3bedeee05d1606f351127d52a78f349a79513f9
Juan Gómez <gomezj01@stepstone.com>
2024-02-20 13:40:40 -0600

    Implementación de un bot de ventas con Next.js

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,18 @@
 import Chat from "@/components/chat/chat";
+import { getProducts } from "@/services/shopify";
+import { createAgent } from "@/utils/openai/createAgent";
 
-export default function ChatPage() {
+export default async function ChatPage() {
+	const products = await getProducts()
+	const productsTitles = products.map(product => product.title)
+	const flatProductsTitle = productsTitles.join('\n')
+
+	const agent = createAgent(flatProductsTitle)
+
 	return(
 		<>
 			<h1>ChatBot</h1>
-			<Chat />
+			<Chat agent={ agent }/>
 		</>
 	)
 }
--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -2,12 +2,21 @@
 
 import { useChat } from 'ai/react';
 
-export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
-	
+export default function Chat(props: { agent: string }) {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+		initialMessages: [{
+			id: '1',
+			role: 'system',
+			content: props.agent
+		}]
+	});
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
+      {
+				messages
+				.filter(m => m.role !== 'system')
+				.map(m => (
         <div key={m.id} className="whitespace-pre-wrap">
           {m.role === 'user' ? 'User: ' : 'AI: '}
           {m.content}
